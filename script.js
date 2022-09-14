565d7e9c9f17abcb6e170f8172df5a90b921d99e
Poonam99 <poonamtabassum9909@gmail.com>
2022-09-15 01:46:16 +0600

    update count

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const modalBackground = document.getElementById("modal-background");
 // variables
 let userText = "";
 let errorCount = 0;
-let startTime;
+let startTime = '';
 let questionText = "";
 
 // Load and display question
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
